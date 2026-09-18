eff349458b89bde1d6719df06584975445aa3c54
akira2963753 <harry2963753@gmail.com>
2026-09-18 13:12:03 +0800

    update

--- 2026-fall-lab01/00_TESTBED/PATTERN.sv
+++ 2026-fall-lab01/00_TESTBED/PATTERN.sv
@@ -21,7 +21,6 @@
 // input mailbox #(txn) gen2drv, [receive the input data from generator]
 // input int unsigned pattern_num
 
-`include "coverage.sv"
 `include "monitor.sv"
 // input virtual vif.monitor_mp mon_if [monitor interface]
 // input mailbox #(mon_txn) mon2scb [monitor the i/o data and send to scoreboard]
@@ -110,7 +109,6 @@ module PATTERN (
         $display("          OISS Environment Completed: %0d Patterns Passed", scb.checked_num);
         $display("                  Checked Property: Ex_cycle");
         $display("================================================================");
-        mon.cov.report();
         $finish;
     end
 
