cd40a20e706971288a740ce751a46dd3b9c230c7
akira2963753 <harry2963753@gmail.com>
2026-09-20 22:03:31 +0800

    update

--- 2026-fall-lab01/03_GATE/file.f
+++
@@ -1,5 +0,0 @@
-+incdir+../00_TESTBED/component/
--sverilog ../02_SYN/Netlist/OISS_SYN.v
--sverilog ../00_TESTBED/PATTERN.sv
--sverilog ../00_TESTBED/TESTBED.v
--v /usr/cad/CBDK_TSMC90G_Arm_v1.1/Lib90/tsmc090.v
--- 2026-fall-lab01/docs/Vplan.md
+++
@@ -1,9 +0,0 @@
-# Verificaiton Plan (Vplan)
-
-##
-
-`TESTBED.sv` 作為 DUT 以及 `PATTERN.sv` (env) 的頂層 Wrapper。
-`PATTERN.sv` 作為 SystemVerilog Platform 的 Environment 角色，負責將所有 componenet 實例化。
-
-
-
